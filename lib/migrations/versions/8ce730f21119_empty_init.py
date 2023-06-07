bdda18b151a57ec58b27d0a7751040864f19a906
Aspen Wilson <aspen.schell@gmail.com>
2023-06-07 08:46:29 -0700

    completed code along

---
+++ lib/migrations/versions/8ce730f21119_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 8ce730f21119
+Revises: 
+Create Date: 2023-06-07 08:26:58.671738
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8ce730f21119'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
